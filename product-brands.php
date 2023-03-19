3c890157623010f4b8b5d0c78604d2e354113500
Shaun Fry <shaun@frydigital.com>
2023-03-19 14:11:32 -0700

    added update checker

--- product-brands.php
+++ product-brands.php
@@ -1,9 +1,11 @@
 <?php
 /*
 Plugin Name: WooCommerce Product Brand
+Plugin URI: https://frydigital.com/plugins/fd-product-brands/
 Description: A WooCommerce plugin that adds a custom taxonomy 'product_brand' to products and allows the attachment of images to the product_brand. Each image links to a search result for that taxonomy.
-Version: 1.0.1
+Version: 1.0.2
 Author: Fry Digital
+Author URI: https://frydigital.com/
 */
 
 // Define the class that extends WooCommerce_Product_Brand
@@ -25,6 +27,31 @@ function my_product_brand_plugin() {
 // Hook the my_product_brand_plugin function to the plugins_loaded hook
 add_action('plugins_loaded', 'my_product_brand_plugin');
 
+/**
+ * 
+ * Plugin update checker
+ * 
+ * Check for Github version release and update
+ * https://github.com/YahnisElsts/plugin-update-checker
+ * 
+ * @since	1.0.2
+ */
 
+ require 'plugin-update-checker/plugin-update-checker.php';
+ use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+ 
+ $myUpdateChecker = PucFactory::buildUpdateChecker(
+   'https://github.com/fryshaun/fd-product-brands/',
+   __FILE__,
+   'fd-product-brands'
+ );
+ //Github Releases
+ $myUpdateChecker->getVcsApi()->enableReleaseAssets();
+ 
+ //Set the branch that contains the stable release.
+ $myUpdateChecker->setBranch('master');
+ 
+ //Optional: If you're using a private repository, specify the access token like this:
+ $myUpdateChecker->setAuthentication('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');
 
 
